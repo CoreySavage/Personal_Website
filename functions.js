8dab23b62ad5729d992ba0a69557cd87b8cd7f17
Corey Savage <Savageco@oregonstate.edu>
2017-10-10 05:35:53 -0400

    Containers update

---
+++ functions.js
@@ -0,0 +1,26 @@
+function projects_toggle() {
+  if (document.getElementById("projects_container").className == "hidden") {
+    document.getElementById("projects_container").className = "";
+  }
+  else {
+    document.getElementById("projects_container").className  = "hidden";
+  }
+}
+
+function about_toggle() {
+  if (document.getElementById("about_container").className == "hidden") {
+    document.getElementById("about_container").className = "";
+  }
+  else {
+    document.getElementById("about_container").className  = "hidden";
+  }
+}
+
+function contact_toggle() {
+  if (document.getElementById("contact_container").className == "hidden") {
+    document.getElementById("contact_container").className = "";
+  }
+  else {
+    document.getElementById("contact_container").className  = "hidden";
+  }
+}
